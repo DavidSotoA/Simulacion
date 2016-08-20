8f936245be62fa3eb9e06bda688fab47d906703c
DavidSotoA <davidsoto17011@hotmail.com>
2016-08-19 22:16:39 -0500

    lab1 v1

---
+++ Lab1/regresionLogistica.m
@@ -0,0 +1,12 @@
+function W = regresionLogistica(X,Y,eta)
+
+[N,D]=size(X);
+W = zeros(D,1);
+
+for iter = 1:100
+    %%% Completar el codigo %%% 
+    F=X.*W;
+    %%% Fin de la modificaci�n %%%
+end
+
+end
