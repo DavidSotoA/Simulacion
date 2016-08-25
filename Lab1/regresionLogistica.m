6054e5052213a8397bf4c62b31fb27061ef5831a
DavidSotoA <davidsoto17011@hotmail.com>
2016-08-24 23:34:10 -0500

    lab1 v4

--- Lab1/regresionLogistica.m
+++ Lab1/regresionLogistica.m
@@ -1,8 +1,24 @@
 function W = regresionLogistica(X,Y,eta)
 
-[N,D]=size(X);
-W = zeros(D,1);
+    [N,D]=size(X);
+    W = zeros(D,1);
+    
+    X1=[];
+    X2=[];
 
+    for i=1:N
+       if(Y(i)==1)
+           X1=[X1;X(i,:)];
+       else
+           X2=[X2;X(i,:)];
+       end
+    end
+    [s1,d1]=size(X1);
+    Z1=zeros(s1,1);
+    
+    [s2,d2]=size(X2);
+    Z2=zeros(s2,1);
+   
     for iter = 1:100
 %          for j=1:D
 %             sum=0;
@@ -19,7 +35,16 @@ W = zeros(D,1);
 %             W(j)=Wj;
 %         end
         
-       W=W-eta.*((1/N).*((sigmoide(X*W)'-Y)'*X))';
+    W=W-eta.*((1/N).*((sigmoide(X*W)'-Y)'*X))';
+
+
+    figure(1)
+% 
+
+     y=-0.5:0.1:6;
+     x = -((y*W(3)+W(2))/W(1));
+     plot(X1(:,1).^2,X1(:,2).^2,'*',X2(:,1).^2,X2(:,2).^2,'o',x,y);
+
     end
 
 end
