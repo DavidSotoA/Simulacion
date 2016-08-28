469bfc62d4f21802659680e3de2e89d8862615ca
DavidSotoA <davidsoto17011@hotmail.com>
2016-08-27 23:09:14 -0500

    lab2 v2

--- Lab2/Algoritmo/ventanaParzen.m
+++ Lab2/Algoritmo/ventanaParzen.m
@@ -21,26 +21,26 @@ function f = ventanaParzen(Xval,Xent,Yent,h,tipo)
     
     elseif strcmp(tipo,'class')
         
-        X1=[];
-        X2=[];
-       
-        for i=1:N
-           if(Yent(i)==1)
-               X1=[X1;Xent(i,:)];
-           else
-               X2=[X2;Xent(i,:)];
-           end
-        end
+        ind1=Yent==1;
+        ind2=Yent==2;
+        ind3=Yent==3;
+        
+        X1=Xent(ind1,:);
+        X2=Xent(ind2,:);
+        X3=Xent(ind3,:);
         
         N1=size(X1,1);
         N2=size(X2,1);
+        N3=size(X3,1);
+        
         fdp1=[];
         fdp2=[];
+        fdp3=[];
         for j=1:M
             sum=0;
             for i=1:N1
-                dist=distancia(Xval(j,:),X1(i,:));
-                k=gaussianKernel(dist/h);
+                dist=(distancia(Xval(j,:),X1(i,:)))/h;
+                k=gaussianKernel(dist);
                 sum=sum+k;
             end
             fdp1=[fdp1;(sum/N1)];
@@ -49,19 +49,32 @@ function f = ventanaParzen(Xval,Xent,Yent,h,tipo)
         for j=1:M 
             sum=0;
             for i=1:N2
-                dist=distancia(Xval(j,:),X2(i,:));
-                k=gaussianKernel(dist/h);
+                dist=(distancia(Xval(j,:),X2(i,:)))/h;
+                k=gaussianKernel(dist);
                 sum=sum+k;
             end
             fdp2=[fdp2;(sum/N2)];
         end
+        
+        for j=1:M 
+            sum=0;
+            for i=1:N3
+                dist=(distancia(Xval(j,:),X3(i,:)))/h;
+                k=gaussianKernel(dist);
+                sum=sum+k;
+            end
+            fdp3=[fdp3;(sum/N3)];
+        end
 
         for i=1:M
-            if(fdp1(i)>fdp2(i))
+            if(fdp1(i)>fdp2(i) && fdp1(i)>fdp3(i))
                 f(i)=1;
+            elseif(fdp2(i)>fdp1(i) && fdp2(i)>fdp3(i))
+                f(i)=2;
             else
-                f(i)=0;
+                 f(i)=3;
             end
+            
         end
         
     end
